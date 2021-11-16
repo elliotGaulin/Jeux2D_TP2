051c3ff184bc927e62c086174476511b56420b3d
2043711 <2043711@etudiant.cegepvicto.ca>
2021-11-16 14:58:34 -0500

    mario dans le niveau

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -1,8 +1,10 @@
 extends Camera2D
 
+onready var Mario = get_parent().get_node("Mario")
 onready var Backgrd1 = get_parent().get_node("BackGrd1")
 onready var Backgrd2 = get_parent().get_node("BackGrd2")
 onready var Backgrd3 = get_parent().get_node("BackGrd3")
+
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -11,12 +13,14 @@ onready var Backgrd3 = get_parent().get_node("BackGrd3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 6000
+	position.x = 0
 
 
 func _process(delta):
-	pass
-	#position.x = position.x + 1
-	#Backgrd1.position.x = Backgrd1.position.x + 0.33333
-	#Backgrd2.position.x = Backgrd2.position.x + 0.33333
-	#Backgrd3.position.x = Backgrd3.position.x + 0.33333
+	if position.x >= 0:
+		position.x = Mario.position.x - 600
+		Backgrd1.position.x = Backgrd1.position.x + 0.33333
+		Backgrd2.position.x = Backgrd2.position.x + 0.33333
+		Backgrd3.position.x = Backgrd3.position.x + 0.33333
+		if position.x < 0:
+			position.x = 0
