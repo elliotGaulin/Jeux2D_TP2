ea9953aaeaba02d17c4892a9871612af9c0447fb
elliotGaulin <elliotgaulin@hotmail.com>
2021-11-16 15:18:03 -0500

    caliss

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -5,7 +5,10 @@ onready var Backgrd1 = get_parent().get_node("BackGrd1")
 onready var Backgrd2 = get_parent().get_node("BackGrd2")
 onready var Backgrd3 = get_parent().get_node("BackGrd3")
 
-
+var last_position_x
+var background1_initial
+var background2_initial
+var background3_initial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -14,13 +17,16 @@ onready var Backgrd3 = get_parent().get_node("BackGrd3")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	position.x = 0
-
+	last_position_x = position.x
+	background1_initial = Backgrd1.position.x	
+	background2_initial = Backgrd2.position.x	
+	background3_initial = Backgrd3.position.x	
 
 func _process(delta):
-	if position.x >= 0:
+	if Mario.position.x >= 600:
 		position.x = Mario.position.x - 600
-		Backgrd1.position.x = Backgrd1.position.x + 0.33333
-		Backgrd2.position.x = Backgrd2.position.x + 0.33333
-		Backgrd3.position.x = Backgrd3.position.x + 0.33333
-		if position.x < 0:
-			position.x = 0
+		if position.x != last_position_x:
+			Backgrd1.position.x = position.x - last_position_x * 0.33333 + background1_initial
+			Backgrd2.position.x = position.x - last_position_x * 0.33333 + background2_initial
+			Backgrd3.position.x = position.x - last_position_x * 0.33333 + background3_initial
+		last_position_x = position.x
