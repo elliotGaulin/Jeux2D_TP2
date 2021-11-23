ad13c95a4c70df1cb4eb81f4b23d2c59d853375c
elliotGaulin <elliotgaulin@hotmail.com>
2021-11-22 22:21:23 -0500

    hahahahahahaahha

---
+++ Scripts/TestMenu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if is_instance_valid($Personnage):
+		if $Personnage.global_position.x > 1950:
+			$Personnage.global_position.x = -200
